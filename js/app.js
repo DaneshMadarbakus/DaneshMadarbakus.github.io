9d887db088492b606ec76e6403e64f092d52a429
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-06-02 02:42:21 +0100

    created sidebar and started about me page

---
+++ js/app.js
@@ -0,0 +1,49 @@
+console.log('working');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Portfolio = Portfolio || {};
+
+// Portfolio.init = function(){
+//   this.$main = $('main');
+//   // $('.aboutMe').on('click', this.aboutMe.bind(this));
+//   $('.projects').on('click', this.projects.bind(this));
+// };
+
+// $(Portfolio.init.bind(Portfolio));
+
+// Portfolio.projects = function(e){
+//   console.log('clicked');
+//   if (e) e.preventDefault();
+//   this.$main.html(`
+//           <h2>General Assembly Web Development Pojects</h2>
+//         <div class="wdiPortfolio">
+//            <div class="col-md-6">
+//             <h4>Fourth Project</h4>
+//             <img class="wdiProjectsIcon" data-toggle="modal" data-target=".suit-modal" src="http://i.imgur.com/g3Hx5sa.png" alt="suit homepage">
+//           </div>
+//           <div class="col-md-6">
+//             <h4>Third Project</h4>
+//             <img class="wdiProjectsIcon" src="http://i.imgur.com/BFWKzyA.png" alt="input homepage">
+//           </div>
+//           <div class="col-md-6">
+//             <h4>Second Project</h4>
+//             <img class="wdiProjectsIcon" src="http://i.imgur.com/Sizr7CC.png" alt="global photos homepage">
+//           </div>
+//           <div class="col-md-6">
+//             <h4>First Project</h4>
+//             <img class="wdiProjectsIcon" src="http://i.imgur.com/vthxHsJ.png" alt="conor mcgregor game homepage">
+//           </div>
+//         </div>
+//   `);
+// };
